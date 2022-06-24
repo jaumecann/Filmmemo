47239f077ede1ed6bf3bdc11d2704dc71900ac80
Jau <jaumecann@gmail.com>
2022-06-24 11:24:05 +0200

    First commit 2.0

---
+++ filmmemo/src/App.js
@@ -0,0 +1,14 @@
+
+import classes from './App.module.css';
+
+function App() {
+  return (
+    <div>
+      <header className={classes.header}>
+        Film<span className={classes.yellow}>History</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
