8f9970ffa31aee8635c6e706fa104454f091256e
Jau <jaumecann@gmail.com>
2022-06-26 11:57:03 +0200

    conditional styling - notas

--- filmmemo/src/components/LastFive/Last5Item.js
+++ filmmemo/src/components/LastFive/Last5Item.js
@@ -2,11 +2,39 @@ import classes from './Last5Item.module.css'
 
 const Last5Item = (props) => {
 
+const rateclasses = () => {
+    console.log(classes.rating)
+
+    let colorstyle = '';
+
+    let gradesToClasses = {
+        9 : "nine",
+        8 : "eight",
+        7 : "seven",
+        6 : "six",
+        5 : "five",
+        4 : "four",
+        3 : "three",
+        2 : "two",
+        1 : "one"
+    }
+
+    colorstyle = gradesToClasses[`${props.rating}`]
+    return `${classes.rating} ${classes[`${colorstyle}`]}` 
+
+    // return `${classes['rating']} ${props.rating > 5 && classes.over} ${props.rating < 5 && classes.under}`
+};
+
+
 return (
     <div className={classes.card}>
+         <div className={classes.poster}>
+            <img src={`/assets/${props.img}.jpg`}></img>
+         </div> 
+         {/* <div className={classes.rating}>{props.rating}</div> */}
+         {/* <div className={`${classes['rating']} ${props.rating > 5 && classes.over} ${props.rating < 5 && classes.under}`}>{props.rating}</div> */}
+         <div className={rateclasses()}>{props.rating}</div>
          <h4>{props.title}</h4>
-         <img src={`/assets/${props.img}.jpg`}></img>
-         
     </div>
 )
 };
