a05a0bcf363d696fc884c31ae8bfda5bdd5cdee0
Jau <jaumecann@gmail.com>
2023-04-06 00:17:10 +0200

    estils

--- filmmemo/src/components/LastFive/LittleCard.js
+++ filmmemo/src/components/LastFive/LittleCard.js
@@ -39,7 +39,7 @@ return (
          <div className={rateclasses()}>{props.rating}</div>
          <div className={classes.ratedate}>{props.date.slice(0,4)}</div>
          <div className={classes.filmdata}>
-            <h4 onClick={()=>{navigateToFilm(props.id)}}>{props.title} </h4>
+            <h4 className={classes.titleh4} onClick={()=>{navigateToFilm(props.id)}}>{props.title} </h4>
             <div className={classes.flag}><img alt='flag' src={`/flags/${props.country}.png`}></img></div>
             <div className={classes.year}>{props.year}</div>
          </div>
