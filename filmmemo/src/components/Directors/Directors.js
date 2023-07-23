d373eaa7028c19fb3d14eb8b9336fb3cd73957e4
Jau <jaumecann@gmail.com>
2023-07-23 23:00:30 +0200

    Una miqueta abans d'anar a dormir

--- filmmemo/src/components/Directors/Directors.js
+++ filmmemo/src/components/Directors/Directors.js
@@ -1,14 +1,35 @@
 import React from "react";
-import classes from './Directors.module.css'
+import classes from './Directors.module.css';
+import { useState } from "react";
 
 const Directors = () => {
+
+    const [bestDirectors, setBestDirector] = useState([]);
+    const [minimum, setMinimum] = useState(1);
+
+    const handleBestDirectorSearch = () => {
+        console.log(minimum)
+    }
+
+    const handleMinimumChange = (event) => {
+        let min = event.target.value
+        if (Number(min)){
+            setMinimum(min)
+        } else if (min && !Number(min)) {
+            alert('el mínim ha de ser un número, tanoca')
+            setMinimum(1)
+        } else {
+            setMinimum(1)
+        }
+        
+    }
 
     return (
         <React.Fragment>
             <div className={classes.commandrow}>
-                <div classname={classes.best_avg}>
-                Minimun films <input classname={classes.best_avg_input}></input>
-                <button>Best rating</button>
+                <div className={classes.best_avg}>
+                Minimun films <input className={classes.best_avg_input} onChange={handleMinimumChange}></input>
+                <button onClick={handleBestDirectorSearch}>Best rating</button>
                 </div>     
             </div>
         </React.Fragment>
