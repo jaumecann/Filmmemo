a84da34ae0421f02588a41a1da2028064dbe7cd5
Jau <jaumecann@gmail.com>
2023-09-11 00:37:22 +0200

    wip filtre paisos a directors

--- filmmemo/src/components/Directors/Directors.js
+++ filmmemo/src/components/Directors/Directors.js
@@ -3,6 +3,8 @@ import classes from './Directors.module.css';
 import { useState,useEffect } from "react";
 import { Pagination } from "@mui/material";
 import FilmrecordContext from '../../shared/context/records-context';
+import  Autocomplete  from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
 const Directors = () => {
 
@@ -13,10 +15,12 @@ const Directors = () => {
     const itemsPerPage = 10;
     const allrecords = React.useContext(FilmrecordContext);
     const [selectedDirectors, setSelectedDirectors] = useState([]);
+    const [countryList, setCountryList] = useState([]);
+    const [selectedCountry, setSelectedCountry] = useState('');
 
     const handleBestDirectorSearch = async () => {
         try{
-           const directors = await fetch(`http://localhost:5000/api/directors/best?min=${minimum}`)
+           const directors = await fetch(`http://localhost:5000/api/directors/best?min=${minimum}&country=${selectedCountry}`)
            const data = await directors.json();
            setBestDirector(data)
         } catch{
@@ -41,6 +45,10 @@ const Directors = () => {
         setPage(value);
     };
 
+    const handleCountryPick = (event, value) => {
+        setSelectedCountry(value.countryid)
+      }
+
     const showFilms = (director, film_ids) => {
         setSelectedDirectors((previous) => {
         if(previous.includes(director)){
@@ -58,29 +66,61 @@ const Directors = () => {
         
     }
 
+    const country = <Autocomplete
+    className={classes.countries}
+    options={countryList}
+    getOptionLabel={(option)=> option.name}
+    isOptionEqualToValue={
+        (option, value) => {
+            return option.countryid === value.countryid
+        }
+    }
+    renderInput={(params) => <TextField {...params} label="Country"/>}
+    name="countryid"
+    onChange={handleCountryPick}
+  />
+
+
+
     useEffect(() => {
         setTotalPages(Math.ceil(bestDirectors.length / itemsPerPage));
         setPage(1);
       }, [bestDirectors]);
+
+      useEffect(()=>{
+        (async() => {
+            const response = await fetch('http://localhost:5000/api/films/countries');
+            const countries = await response.json();
+            setCountryList(countries);
+        })();
+    
+    },[]);
 
     return (
         <React.Fragment>
             <div className={classes.commandrow}>
                 <div className={classes.best_avg}>
                 Minimun films <input className={classes.best_avg_input} onChange={handleMinimumChange}></input>
+                <div className={classes.country_input}>
+                {country}
+                </div>
                 <button  onClick={handleBestDirectorSearch}>Best rating</button>
                 </div>     
+                
             </div>
+            
             <div className={classes.list}>
-                {bestDirectors.slice((page-1)*itemsPerPage, page*itemsPerPage).map(item => 
-                <div key={item.directorid} className={classes.datarow} 
-                onClick={() => showFilms(item.directorid, item.film_ids)}> 
-                <b className={classes.directorname}>{item.directorname}</b><span className={classes.avg}>{item.avrg.toFixed(2)}</span><span className={classes.totals}>{item.totalfilms}</span><span className={classes.country}><img alt='flag' src={`/flags/${item.directorcountry.trim()}.png`}></img></span>
-                {selectedDirectors.includes(item.directorid) && <div className={classes.postercontainer}>
-                    {allrecords.collection.filter((record)=>{return record.directorid === item.directorid}).map(item => <div className={classes.posterlist} key={item.id}>
-                        <img alt='poster' src={`/assets/${item.poster}`}></img>
-                        <div className={classes.posterraiting}>{item.rating}</div>
-                    </div>)}
+                {bestDirectors.slice((page-1)*itemsPerPage, page*itemsPerPage).map((item, idx) => 
+                    <div key={item.directorid} className={classes.datarow} 
+                    onClick={() => showFilms(item.directorid, item.film_ids)}> 
+                        <span>{idx+1}</span><b className={classes.directorname}> {item.directorname}</b><span className={classes.avg}>{item.avrg.toFixed(2)}</span><span className={classes.totals}>{item.totalfilms}</span><span className={classes.country}><img alt='flag' src={`/flags/${item.directorcountry.trim()}.png`}></img></span>
+                    {selectedDirectors.includes(item.directorid) && <div className={classes.postercontainer}>
+                        {allrecords.collection.filter((record)=>{return record.directorid === item.directorid}).map(item => 
+                            <div className={classes.posterlist} key={item.id}>
+                                <img alt='poster' src={`/assets/${item.poster}`}></img>
+                                <div className={classes.posterraiting}>{item.rating}</div>
+                            </div>)
+                        }
                 </div>}
                 </div>
                 )
