85ba8e56c7e3d572c63997d7ccef0f38bb373670
Jau <jaumecann@gmail.com>
2024-10-26 19:01:20 +0200

    secció anys de contry facts

--- filmmemo/src/components/CountryFacts/CountryFactsModal.js
+++ filmmemo/src/components/CountryFacts/CountryFactsModal.js
@@ -5,6 +5,7 @@ const FactsModal = ({openModal, country}) => {
 
     const [countryFacts, setCountryFacts] = useState()
     const [daysDiff, setDaysDiff] = useState();
+    const [yearData, setYearData] = useState([]);
 
     useEffect(()=>{
         
@@ -12,7 +13,9 @@ const FactsModal = ({openModal, country}) => {
             const response = await fetch(`http://localhost:5000/api/films/getCountryFacts/?ctry=${country}`);
             const info = await response.json();
             setCountryFacts(info)
-            getSince(info?.last?.ratedate)
+            getSince(info?.last?.ratedate);
+            getYearStats(info?.list)
+            
         })();
     },[country])
 
@@ -20,14 +23,33 @@ const FactsModal = ({openModal, country}) => {
         const dataInicial = new Date(ratedate);
         const dataActual = new Date();
         const diff = dataActual - dataInicial;
-        const daysGOne = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const daysGOne = Math.floor(diff / 86400000);
         setDaysDiff(daysGOne)
+    }
+
+    const getYearStats = (list) => {
+        let yearObject = {};
+        list.map(l => {
+            const data = new Date(l.ratedate);
+            const year = data.getFullYear();
+            if(!yearObject[year]){
+                yearObject[year] = 1;
+            } else {
+                yearObject[year] += 1;
+            }
+        })
+        const years = Object.entries(yearObject).map(([clau, valor]) => {
+            return { [clau]: valor * 2 };
+          });
+
+        setYearData(years)
     }
 
 
     return (<div className={`${classes.modal} ${openModal ? classes.opening : ''}`}>
         <img alt="flag" src={`/flags/${country.trim()}.png`}></img>
         <div>{countryFacts?.stats?.name}</div>
+
         <section className={classes.databox}>
             <div className={classes.colddata}>
                 <p>Total films: {countryFacts?.stats?.record_count}</p>
@@ -35,6 +57,7 @@ const FactsModal = ({openModal, country}) => {
                 <p>Frequency: {countryFacts?.stats?.percentage}%</p>
             </div>
         </section>
+
         <section className={classes.lastseen}>
         <div className={classes.img_last}>
         <img alt="flag" src={`/assets/${countryFacts?.last?.poster}`}></img>
@@ -45,6 +68,11 @@ const FactsModal = ({openModal, country}) => {
             <p>Fa {daysDiff} dies</p>
         </div>
         </section>
+
+        <section>
+
+        </section>
+
     </div>)
 }
 
