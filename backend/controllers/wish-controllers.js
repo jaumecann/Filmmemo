03dc9046f49f1ffbb70ca47dbf1c6449f3c27327
Jau <jaumecann@gmail.com>
2024-11-24 23:57:56 +0100

    wishlist backend

---
+++ backend/controllers/wish-controllers.js
@@ -0,0 +1,2 @@
+const sqlquery = require('../utils/database');
+
---
+++ backend/routes/wishlist-routes.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const wishControllers = require('../controllers/wish-controllers')
+
+// const router = express.Router();
+
+// router.get('/wishlist', wishControllers.getWishlist);
+
+
+// module.exports = router
